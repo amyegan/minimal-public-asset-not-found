9a509a55f75d5c6c5cae181f21d4c0bcac72f362
mattsawka <matthewsawka@gmail.com>
2023-01-10 11:47:14 -0500

    add dev

--- pages/api/sizeCheckAPI/[hello].ts
+++ pages/api/sizeCheckAPI/[hello].ts
@@ -1,14 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
 var fs = require("fs"); // Load the filesystem module
-
+const env = process.env.NODE_ENV;
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   var fileSizeInBytes = 0;
+
+  var finalFontFilePath,
+    finalImageFilePath = "";
+
+  if (env === "development") {
+    finalFontFilePath = "public/Roboto-Regular.ttf"; // for dev / localhost only;
+    finalImageFilePath = "public/next.svg"; // for dev / localhost only
+  } else {
+    console.log("In Environment: ", env);
+    finalFontFilePath = path.join(process.cwd(), "Roboto-Regular.ttf");
+    finalImageFilePath = path.join(process.cwd(), "next.svg");
+  }
+
   try {
-    var stats = fs.statSync("/Roboto-Regular.ttf");
+    var stats = fs.statSync(finalFontFilePath);
 
     if (req.query.hello === "imageFile") {
-      stats = fs.statSync("/next.svg");
+      stats = fs.statSync(finalImageFilePath);
     }
 
     fileSizeInBytes = stats.size;
